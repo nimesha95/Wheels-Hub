8fefb3fa7f7c78c554df4ca544836df46400f1be
nimesha95 <nimesha95@live.com>
2018-04-15 14:40:22 +0530

    vehicle registeration is done

--- client/routes/index.js
+++ client/routes/index.js
@@ -12,13 +12,13 @@ const app = { user: null, keys: [], assets: [], transfers: [] }
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function (req, res) {
-	if(req.user.user_type=="rmv"){
+	if (req.user.user_type == "rmv") {
 		res.render('RMV/index');
 	}
-	else if(req.user.user_type=="insurance"){
+	else if (req.user.user_type == "insurance") {
 		res.render('insurance');
 	}
-	else if(req.user.user_type=="gen_user"){
+	else if (req.user.user_type == "gen_user") {
 		res.render('gen_user');
 	}
 });
@@ -45,25 +45,31 @@ function ensureAuthenticated(req, res, next) {
 
 
 router.post('/register_vehicle', function (req, res) {
-	RegVehicle(req,req.body.vehicle_no,req.body.chasis_no);
+	RegVehicle(req, req.body.vehicle_no, req.body.chasis_no);
 	//req.flash('success_msg', req.body.vehicle_no);
 	res.redirect('/register_vehicle');
 });
 
 
-function RegVehicle(req,asset,vehicle_info){
-	
+function RegVehicle(req, asset, vehicle_info) {
+
 
 	//var asset = req.body.vehicle_no;
-	var vehicle_info = {"vehicle": {"chasis_no": req.body.chasis_no, "testing": "testing"}}
+	var vehicle_info = {
+		"vehicle": {
+			"chasis_no": req.body.chasis_no, "vehicle_no": req.body.vehicle_no,
+			"Model": req.body.Model, "yom": req.body.yom, "manufacture_country": req.body.manufacture_country, 
+			"engine_no": req.body.engine_no, "color": req.body.color
+		}
+	}
 	//vehicle_info = JSON.stringify(vehicle_info)
 	req.flash('success_msg', vehicle_info.vehicle);
 	submitUpdate(
-		{ action: 'create', asset , vehicle_info, owner: req.user.public_key },
+		{ action: 'create', asset, vehicle_info, owner: req.user.public_key },
 		req.user.private_key,
 		success => success ? this.refresh() : null
 	)
-	
+
 	/*
 	submitUpdate(
 		{ action: 'create', asset , chasis_no, owner: req.user.public_key },
